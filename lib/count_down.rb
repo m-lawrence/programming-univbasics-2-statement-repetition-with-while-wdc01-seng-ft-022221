d295c0af03420111e348bd7e007b926c0792e55e
Melissa Lawrence <mlawrence537@gmail.com>
2021-01-16 23:04:29 +0000

    Automatically backed up by Learn

--- lib/count_down.rb
+++ lib/count_down.rb
@@ -1,9 +1,7 @@
 
 count = 10
 
-while count > 0 do
-  puts count
-  break if count == 0 
-    puts "Happy New Year!"
+while count >= 0 do
+  puts count unless count == 0 puts "Happy New Year!"
   count -= 1
 end
